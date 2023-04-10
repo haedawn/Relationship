18c84a78f9be17e886e5646f12ea64c54e62b14a
Woorim Kim <wrkimlimi@gmail.com>
2023-04-10 20:18:33 +0900

    during session

--- session/users/views.py
+++ session/users/views.py
@@ -1,40 +1,72 @@
 from django.contrib import auth
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
 from .models import Profile
 from django.shortcuts import render, redirect
+from .forms import CustomUserSignupForm, CustomUserSigninForm
 
-# 회원가입
+
 def signup(request):
-    if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                username=request.POST['username'],
-                password=request.POST['password1'],
-                email=request.POST['email'],)
-            profile = Profile(user=user, nickname=request.POST['nickname'], image=request.FILES.get('profile_image'))
-            profile.save()
+    form = CustomUserSignupForm()
+    #착한 사용자
+    if request.method == "POST":
+        CustomUserSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
 
-            auth.login(request, user)
-            return redirect('/')
-        return render(request, 'signup.html')
-    return render(request, 'signup.html')
+    #나쁜 사용자
+    return render(request, "newSignup.html", {"form": form})
 
-# 로그인
-def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'login.html')
-    else:
-        return render(request, 'login.html')
 
-# 로그아웃
-def logout(request):
-  auth.logout(request)
-  return redirect('home')
+def signin(request):
+    form = CustomUserSigninForm()
+    #착한 사용자
+    if request.method == "POST":
+        form = CustomUserSigninForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("home")
+    #나쁜 사용자
+    return render(request, "newSignin.html", {"form": form})
+
+
+def signout(request):
+    logout(request)
+    return redirect("home")
+
+# # 회원가입
+# def signup(request):
+#     if request.method == 'POST':
+#         if request.POST['password1'] == request.POST['password2']:
+#             user = User.objects.create_user(
+#                 username=request.POST['username'],
+#                 password=request.POST['password1'],
+#                 email=request.POST['email'],)
+#             profile = Profile(user=user, nickname=request.POST['nickname'], image=request.FILES.get('profile_image'))
+#             profile.save()
+
+#             auth.login(request, user)
+#             return redirect('/')
+#         return render(request, 'signup.html')
+#     return render(request, 'signup.html')
+
+# # 로그인
+# def login(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             auth.login(request, user)
+#             return redirect('/')
+#         else:
+#             return render(request, 'login.html')
+#     else:
+#         return render(request, 'login.html')
+
+# # 로그아웃
+# def logout(request):
+#   auth.logout(request)
+#   return redirect('home')
+
